9ab200aa7475714b52a0325c31ac516de0316de0
SRCoughlin <sean@froth.com>
2018-09-17 19:46:59 -0400

    Added merkle_tree tests

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,17 @@
 #include <libsnark/relations/constraint_satisfaction_problems/r1cs/examples/r1cs_examples.hpp>
 #include <libsnark/zk_proof_systems/ppzksnark/r1cs_gg_ppzksnark/r1cs_gg_ppzksnark.hpp>
 
+#ifdef CURVE_BN128
+#include <libff/algebra/curves/bn128/bn128_pp.hpp>
+#endif
+#include <libff/algebra/curves/edwards/edwards_pp.hpp>
+#include <libff/algebra/curves/mnt/mnt4/mnt4_pp.hpp>
+#include <libff/algebra/curves/mnt/mnt6/mnt6_pp.hpp>
+
+#include <libsnark/gadgetlib1/gadgets/hashes/sha256/sha256_gadget.hpp>
+#include <libsnark/gadgetlib1/gadgets/merkle_tree/merkle_tree_check_read_gadget.hpp>
+#include <libsnark/gadgetlib1/gadgets/merkle_tree/merkle_tree_check_update_gadget.hpp>
+
 using namespace libsnark;
 
 /**
@@ -51,9 +62,37 @@ void test_r1cs_gg_ppzksnark(size_t num_constraints, size_t input_size)
     assert(bit);
 }
 
+
+template<typename ppT>
+void test_all_merkle_tree_gadgets()
+{
+    typedef libff::Fr<ppT> FieldT;
+    test_merkle_tree_check_read_gadget<FieldT, CRH_with_bit_out_gadget<FieldT> >();
+    test_merkle_tree_check_read_gadget<FieldT, sha256_two_to_one_hash_gadget<FieldT> >();
+
+    test_merkle_tree_check_update_gadget<FieldT, CRH_with_bit_out_gadget<FieldT> >();
+    test_merkle_tree_check_update_gadget<FieldT, sha256_two_to_one_hash_gadget<FieldT> >();
+}
+
 int main () {
     default_r1cs_gg_ppzksnark_pp::init_public_params();
     test_r1cs_gg_ppzksnark<default_r1cs_gg_ppzksnark_pp>(1000, 100);
+
+    libff::start_profiling();
+
+#ifdef CURVE_BN128       // BN128 has fancy dependencies so it may be disabled
+    libff::bn128_pp::init_public_params();
+    test_all_merkle_tree_gadgets<libff::bn128_pp>();
+#endif
+
+    libff::edwards_pp::init_public_params();
+    test_all_merkle_tree_gadgets<libff::edwards_pp>();
+
+    libff::mnt4_pp::init_public_params();
+    test_all_merkle_tree_gadgets<libff::mnt4_pp>();
+
+    libff::mnt6_pp::init_public_params();
+    test_all_merkle_tree_gadgets<libff::mnt6_pp>();
 
     return 0;
 }
