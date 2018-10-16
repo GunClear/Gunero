87ee4156ee149c8556eb5a96bd3c6f909934b714
Sean Coughlin <sean@froth.com>
2018-10-16 19:06:07 -0400

    Preparing for implementation of whitelist/proof of membership

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
-//#include <libff/common/default_types/ec_pp.hpp>
+//#include <libff/common/default_types/ec_pp.hpp> 
+#include </home/sean/Gunero/depends/libsnark/libsnark/common/default_types/r1cs_gg_ppzksnark_pp.hpp>
 #include <libsnark/common/default_types/r1cs_gg_ppzksnark_pp.hpp>
 #include <libsnark/relations/constraint_satisfaction_problems/r1cs/examples/r1cs_examples.hpp>
 #include <libsnark/zk_proof_systems/ppzksnark/r1cs_gg_ppzksnark/r1cs_gg_ppzksnark.hpp>
@@ -222,10 +223,9 @@ void Gunero_test_merkle_tree_check_read_gadget(size_t tree_depth)
     libff::print_header("Gunero verify");
     {
         r1cs_ppzksnark_verification_key<BaseT> vk;
-
         loadFromFile(vkPath, vk);
 
-        auto vk_precomp = r1cs_ppzksnark_verifier_process_vk(vk);
+        r1cs_ppzksnark_processed_verification_key<BaseT> vk_precomp = r1cs_ppzksnark_verifier_process_vk(vk);
     }
 
     /* make sure that read checker didn't accidentally overwrite anything */
@@ -242,87 +242,514 @@ void Gunero_test_merkle_tree_check_read_gadget(size_t tree_depth)
     libff::clear_profiling_counters();
 }
 
-// template<typename FieldT, typename HashT>
-// void Gunero_test_sparse_merkle_tree_check_read_gadget()
-// {
-//     /* prepare test variables */
-//     std::clock_t    start = std::clock();
-//     libff::print_header("Gunero prepare test variables");
-//     const size_t digest_len = HashT::get_digest_len();
-//     const size_t tree_depth = 8;
-//     std::vector<merkle_authentication_node> path(tree_depth);
+const unsigned char G1_PREFIX_MASK = 0x02;
+const unsigned char G2_PREFIX_MASK = 0x0a;
 
-//     libff::bit_vector prev_hash(digest_len);
-//     std::generate(prev_hash.begin(), prev_hash.end(), [&]() { return std::rand() % 2; });
-//     libff::bit_vector leaf = prev_hash;
+// Element in the base field
+class Fq {
+private:
+    base_blob<256> data;
+public:
+    Fq() : data() { }
 
-//     libff::bit_vector address_bits;
+    template<typename libsnark_Fq>
+    Fq(libsnark_Fq element);
 
-//     size_t address = 0;
-//     for (long level = tree_depth-1; level >= 0; --level)
-//     {
-//         const bool computed_is_right = (std::rand() % 2);
-//         address |= (computed_is_right ? 1ul << (tree_depth-1-level) : 0);
-//         address_bits.push_back(computed_is_right);
-//         libff::bit_vector other(digest_len);
+    template<typename libsnark_Fq>
+    libsnark_Fq to_libsnark_fq() const;
 
-//         //Decide on sparseness (with the computed_is_right ignored on read but used for update)
-//         if (std::rand() % 2)
-//         {//sparse/NULL
-//             std::generate(other.begin(), other.end(), [&]() { return 0; });
-//         }
-//         else
-//         {
-//              std::generate(other.begin(), other.end(), [&]() { return std::rand() % 2; });
-//         }
+    ADD_SERIALIZE_METHODS;
 
-//         libff::bit_vector block = prev_hash;
-//         block.insert(computed_is_right ? block.begin() : block.end(), other.begin(), other.end());
-//         libff::bit_vector h = HashT::get_hash(block);
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
+        READWRITE(data);
+    }
 
-//         path[level] = other;
+    friend bool operator==(const Fq& a, const Fq& b)
+    {
+        return (
+            a.data == b.data
+        );
+    }
 
-//         prev_hash = h;
-//     }
-//     libff::bit_vector root = prev_hash;
-//     printf("\n"); libff::print_indent(); libff::print_mem("after prepare test variables"); libff::print_time("after prepare test variables");
+    friend bool operator!=(const Fq& a, const Fq& b)
+    {
+        return !(a == b);
+    }
+};
 
-//     /* generate circuit */
-//     libff::print_header("Gunero Generator");
-//     protoboard<FieldT> pb;
-//     pb_variable_array<FieldT> address_bits_va;
-//     address_bits_va.allocate(pb, tree_depth, "address_bits");
-//     digest_variable<FieldT> leaf_digest(pb, digest_len, "input_block");
-//     digest_variable<FieldT> root_digest(pb, digest_len, "output_digest");
-//     merkle_authentication_path_variable<FieldT, HashT> path_var(pb, tree_depth, "path_var");
-//     gunero::sparse_merkle_tree_check_read_gadget<FieldT, HashT> ml(pb, tree_depth, address_bits_va, leaf_digest, root_digest, path_var, ONE, "ml");
+// Element in the extension field
+class Fq2 {
+private:
+    base_blob<512> data;
+public:
+    Fq2() : data() { }
 
-//     path_var.generate_r1cs_constraints();
-//     ml.generate_r1cs_constraints();
-//     printf("\n"); libff::print_indent(); libff::print_mem("after generator"); libff::print_time("after generator");
+    template<typename libsnark_Fq2>
+    Fq2(libsnark_Fq2 element);
 
-//     /* witness (proof) */
-//     libff::print_header("Gunero witness (proof)");
-//     address_bits_va.fill_with_bits(pb, address_bits);
-//     assert(address_bits_va.get_field_element_from_bits(pb).as_ulong() == address);
-//     leaf_digest.generate_r1cs_witness(leaf);
-//     path_var.generate_r1cs_witness(address, path);
-//     ml.generate_r1cs_witness();
+    template<typename libsnark_Fq2>
+    libsnark_Fq2 to_libsnark_fq2() const;
 
-//     /* make sure that read checker didn't accidentally overwrite anything */
-//     address_bits_va.fill_with_bits(pb, address_bits);
-//     leaf_digest.generate_r1cs_witness(leaf);
-//     root_digest.generate_r1cs_witness(root);
-//     printf("\n"); libff::print_indent(); libff::print_mem("after witness (proof)"); libff::print_time("after witness (proof)");
+    ADD_SERIALIZE_METHODS;
 
-//     /* verify */
-//     libff::print_header("Gunero verify");
-//     assert(pb.is_satisfied());
-//     const size_t num_constraints = pb.num_constraints();
-//     const size_t expected_constraints = gunero::sparse_merkle_tree_check_read_gadget<FieldT, HashT>::expected_constraints(tree_depth);
-//     assert(num_constraints == expected_constraints);
-//     printf("\n"); libff::print_indent(); libff::print_mem("after verify"); libff::print_time("after verify");
-// }
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
+        READWRITE(data);
+    }
+
+    friend bool operator==(const Fq2& a, const Fq2& b)
+    {
+        return (
+            a.data == b.data
+        );
+    }
+
+    friend bool operator!=(const Fq2& a, const Fq2& b)
+    {
+        return !(a == b);
+    }
+};
+
+// Compressed point in G1
+class CompressedG1 {
+private:
+    bool y_lsb;
+    Fq x;
+
+public:
+    CompressedG1() : y_lsb(false), x() { }
+
+    template<typename libsnark_G1>
+    CompressedG1(libsnark_G1 point);
+
+    template<typename libsnark_G1>
+    libsnark_G1 to_libsnark_g1() const;
+
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
+        unsigned char leadingByte = G1_PREFIX_MASK;
+
+        if (y_lsb) {
+            leadingByte |= 1;
+        }
+
+        READWRITE(leadingByte);
+
+        if ((leadingByte & (~1)) != G1_PREFIX_MASK) {
+            throw std::ios_base::failure("lead byte of G1 point not recognized");
+        }
+
+        y_lsb = leadingByte & 1;
+
+        READWRITE(x);
+    }
+
+    friend bool operator==(const CompressedG1& a, const CompressedG1& b)
+    {
+        return (
+            a.y_lsb == b.y_lsb &&
+            a.x == b.x
+        );
+    }
+
+    friend bool operator!=(const CompressedG1& a, const CompressedG1& b)
+    {
+        return !(a == b);
+    }
+};
+
+// Compressed point in G2
+class CompressedG2 {
+private:
+    bool y_gt;
+    Fq2 x;
+
+public:
+    CompressedG2() : y_gt(false), x() { }
+
+    template<typename libsnark_G2>
+    CompressedG2(libsnark_G2 point);
+
+    template<typename libsnark_G2>
+    libsnark_G2 to_libsnark_g2() const;
+
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
+        unsigned char leadingByte = G2_PREFIX_MASK;
+
+        if (y_gt) {
+            leadingByte |= 1;
+        }
+
+        READWRITE(leadingByte);
+
+        if ((leadingByte & (~1)) != G2_PREFIX_MASK) {
+            throw std::ios_base::failure("lead byte of G2 point not recognized");
+        }
+
+        y_gt = leadingByte & 1;
+
+        READWRITE(x);
+    }
+
+    friend bool operator==(const CompressedG2& a, const CompressedG2& b)
+    {
+        return (
+            a.y_gt == b.y_gt &&
+            a.x == b.x
+        );
+    }
+
+    friend bool operator!=(const CompressedG2& a, const CompressedG2& b)
+    {
+        return !(a == b);
+    }
+};
+
+// Compressed zkSNARK proof
+class ZCProof {
+private:
+    CompressedG1 g_A;
+    CompressedG1 g_A_prime;
+    CompressedG2 g_B;
+    CompressedG1 g_B_prime;
+    CompressedG1 g_C;
+    CompressedG1 g_C_prime;
+    CompressedG1 g_K;
+    CompressedG1 g_H;
+
+public:
+    ZCProof() : g_A(), g_A_prime(), g_B(), g_B_prime(), g_C(), g_C_prime(), g_K(), g_H() { }
+
+    // Produces a compressed proof using a libsnark zkSNARK proof
+    template<typename libsnark_proof>
+    ZCProof(const libsnark_proof& proof);
+
+    // Produces a libsnark zkSNARK proof out of this proof,
+    // or throws an exception if it is invalid.
+    template<typename libsnark_proof>
+    libsnark_proof to_libsnark_proof() const;
+
+    static ZCProof random_invalid();
+
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
+        READWRITE(g_A);
+        READWRITE(g_A_prime);
+        READWRITE(g_B);
+        READWRITE(g_B_prime);
+        READWRITE(g_C);
+        READWRITE(g_C_prime);
+        READWRITE(g_K);
+        READWRITE(g_H);
+    }
+
+    friend bool operator==(const ZCProof& a, const ZCProof& b)
+    {
+        return (
+            a.g_A == b.g_A &&
+            a.g_A_prime == b.g_A_prime &&
+            a.g_B == b.g_B &&
+            a.g_B_prime == b.g_B_prime &&
+            a.g_C == b.g_C &&
+            a.g_C_prime == b.g_C_prime &&
+            a.g_K == b.g_K &&
+            a.g_H == b.g_H
+        );
+    }
+
+    friend bool operator!=(const ZCProof& a, const ZCProof& b)
+    {
+        return !(a == b);
+    }
+};
+
+template<typename FieldT, typename BaseT, typename HashT, size_t tree_depth>
+class GuneroMembershipCircuit
+{
+public:
+    r1cs_ppzksnark_proving_key<BaseT> pk;
+    r1cs_ppzksnark_verification_key<BaseT> vk;
+    r1cs_ppzksnark_processed_verification_key<BaseT> vk_precomp;
+    std::string pkPath;
+
+    GuneroMembershipCircuit() {}
+    ~GuneroMembershipCircuit() {}
+
+    void setProvingKeyPath(std::string path) {
+        pkPath = path;
+    }
+
+    void loadProvingKey() {
+        if (!pk) {
+            loadFromFile(pkPath, pk);
+        }
+    }
+
+    void saveProvingKey(std::string path) {
+        if (pk) {
+            saveToFile(path, pk);
+        } else {
+            throw std::runtime_error("cannot save proving key; key doesn't exist");
+        }
+    }
+    void loadVerifyingKey(std::string path) {
+        loadFromFile(path, vk);
+
+        processVerifyingKey();
+    }
+    void processVerifyingKey() {
+        vk_precomp = r1cs_ppzksnark_verifier_process_vk(*vk);
+    }
+    void saveVerifyingKey(std::string path) {
+        if (vk) {
+            saveToFile(path, *vk);
+        } else {
+            throw std::runtime_error("cannot save verifying key; key doesn't exist");
+        }
+    }
+    void saveR1CS(std::string path) {
+        auto r1cs = generate_r1cs();
+
+        saveToFile(path, r1cs);
+    }
+
+    r1cs_constraint_system<FieldT> generate_r1cs() {
+        protoboard<FieldT> pb;
+
+        guneromembership_gadget<FieldT, BaseT, HashT, tree_depth> g(pb);
+        g.generate_r1cs_constraints();
+
+        return pb.get_constraint_system();
+    }
+
+    void generate() {
+        const r1cs_constraint_system<FieldT> constraint_system = generate_r1cs();
+        r1cs_ppzksnark_keypair<BaseT> keypair = r1cs_ppzksnark_generator<BaseT>(constraint_system);
+
+        pk = keypair.pk;
+        vk = keypair.vk;
+        processVerifyingKey();
+    }
+
+    bool verify(
+        const ZCProof& proof,
+        ProofVerifier& verifier,
+        const uint256& pubKeyHash,
+        const uint256& randomSeed,
+        const boost::array<uint256, NumInputs>& macs,
+        const boost::array<uint256, NumInputs>& nullifiers,
+        const boost::array<uint256, NumOutputs>& commitments,
+        uint64_t vpub_old,
+        uint64_t vpub_new,
+        const uint256& rt
+    ) {
+        if (!vk || !vk_precomp) {
+            throw std::runtime_error("JoinSplit verifying key not loaded");
+        }
+
+        try {
+            auto r1cs_proof = proof.to_libsnark_proof<r1cs_ppzksnark_proof<ppzksnark_ppT>>();
+
+            uint256 h_sig = this->h_sig(randomSeed, nullifiers, pubKeyHash);
+
+            auto witness = joinsplit_gadget<FieldT, NumInputs, NumOutputs>::witness_map(
+                rt,
+                h_sig,
+                macs,
+                nullifiers,
+                commitments,
+                vpub_old,
+                vpub_new
+            );
+
+            return verifier.check(
+                *vk,
+                *vk_precomp,
+                witness,
+                r1cs_proof
+            );
+        } catch (...) {
+            return false;
+        }
+    }
+
+    ZCProof prove(
+        const boost::array<JSInput, NumInputs>& inputs,
+        const boost::array<JSOutput, NumOutputs>& outputs,
+        boost::array<Note, NumOutputs>& out_notes,
+        boost::array<ZCNoteEncryption::Ciphertext, NumOutputs>& out_ciphertexts,
+        uint256& out_ephemeralKey,
+        const uint256& pubKeyHash,
+        uint256& out_randomSeed,
+        boost::array<uint256, NumInputs>& out_macs,
+        boost::array<uint256, NumInputs>& out_nullifiers,
+        boost::array<uint256, NumOutputs>& out_commitments,
+        uint64_t vpub_old,
+        uint64_t vpub_new,
+        const uint256& rt,
+        bool computeProof
+    ) {
+        if (computeProof && !pk) {
+            throw std::runtime_error("JoinSplit proving key not loaded");
+        }
+
+        if (vpub_old > MAX_MONEY) {
+            throw std::invalid_argument("nonsensical vpub_old value");
+        }
+
+        if (vpub_new > MAX_MONEY) {
+            throw std::invalid_argument("nonsensical vpub_new value");
+        }
+
+        uint64_t lhs_value = vpub_old;
+        uint64_t rhs_value = vpub_new;
+
+        for (size_t i = 0; i < NumInputs; i++) {
+            // Sanity checks of input
+            {
+                // If note has nonzero value
+                if (inputs[i].note.value != 0) {
+                    // The witness root must equal the input root.
+                    if (inputs[i].witness.root() != rt) {
+                        throw std::invalid_argument("joinsplit not anchored to the correct root");
+                    }
+
+                    // The tree must witness the correct element
+                    if (inputs[i].note.cm() != inputs[i].witness.element()) {
+                        throw std::invalid_argument("witness of wrong element for joinsplit input");
+                    }
+                }
+
+                // Ensure we have the key to this note.
+                if (inputs[i].note.a_pk != inputs[i].key.address().a_pk) {
+                    throw std::invalid_argument("input note not authorized to spend with given key");
+                }
+
+                // Balance must be sensical
+                if (inputs[i].note.value > MAX_MONEY) {
+                    throw std::invalid_argument("nonsensical input note value");
+                }
+
+                lhs_value += inputs[i].note.value;
+
+                if (lhs_value > MAX_MONEY) {
+                    throw std::invalid_argument("nonsensical left hand size of joinsplit balance");
+                }
+            }
+
+            // Compute nullifier of input
+            out_nullifiers[i] = inputs[i].nullifier();
+        }
+
+        // Sample randomSeed
+        out_randomSeed = random_uint256();
+
+        // Compute h_sig
+        uint256 h_sig = this->h_sig(out_randomSeed, out_nullifiers, pubKeyHash);
+
+        // Sample phi
+        uint252 phi = random_uint252();
+
+        // Compute notes for outputs
+        for (size_t i = 0; i < NumOutputs; i++) {
+            // Sanity checks of output
+            {
+                if (outputs[i].value > MAX_MONEY) {
+                    throw std::invalid_argument("nonsensical output value");
+                }
+
+                rhs_value += outputs[i].value;
+
+                if (rhs_value > MAX_MONEY) {
+                    throw std::invalid_argument("nonsensical right hand side of joinsplit balance");
+                }
+            }
+
+            // Sample r
+            uint256 r = random_uint256();
+
+            out_notes[i] = outputs[i].note(phi, r, i, h_sig);
+        }
+
+        if (lhs_value != rhs_value) {
+            throw std::invalid_argument("invalid joinsplit balance");
+        }
+
+        // Compute the output commitments
+        for (size_t i = 0; i < NumOutputs; i++) {
+            out_commitments[i] = out_notes[i].cm();
+        }
+
+        // Encrypt the ciphertexts containing the note
+        // plaintexts to the recipients of the value.
+        {
+            ZCNoteEncryption encryptor(h_sig);
+
+            for (size_t i = 0; i < NumOutputs; i++) {
+                NotePlaintext pt(out_notes[i], outputs[i].memo);
+
+                out_ciphertexts[i] = pt.encrypt(encryptor, outputs[i].addr.pk_enc);
+            }
+
+            out_ephemeralKey = encryptor.get_epk();
+        }
+
+        // Authenticate h_sig with each of the input
+        // spending keys, producing macs which protect
+        // against malleability.
+        for (size_t i = 0; i < NumInputs; i++) {
+            out_macs[i] = PRF_pk(inputs[i].key, i, h_sig);
+        }
+
+        if (!computeProof) {
+            return ZCProof();
+        }
+
+        protoboard<FieldT> pb;
+        {
+            joinsplit_gadget<FieldT, NumInputs, NumOutputs> g(pb);
+            g.generate_r1cs_constraints();
+            g.generate_r1cs_witness(
+                phi,
+                rt,
+                h_sig,
+                inputs,
+                out_notes,
+                vpub_old,
+                vpub_new
+            );
+        }
+
+        // The constraint system must be satisfied or there is an unimplemented
+        // or incorrect sanity check above. Or the constraint system is broken!
+        assert(pb.is_satisfied());
+
+        // TODO: These are copies, which is not strictly necessary.
+        std::vector<FieldT> primary_input = pb.primary_input();
+        std::vector<FieldT> aux_input = pb.auxiliary_input();
+
+        // Swap A and B if it's beneficial (less arithmetic in G2)
+        // In our circuit, we already know that it's beneficial
+        // to swap, but it takes so little time to perform this
+        // estimate that it doesn't matter if we check every time.
+        pb.constraint_system.swap_AB_if_beneficial();
+
+        return ZCProof(r1cs_ppzksnark_prover<ppzksnark_ppT>(
+            *pk,
+            primary_input,
+            aux_input,
+            pb.constraint_system
+        ));
+    }
+};
 
 int main () {
 //    default_r1cs_gg_ppzksnark_pp::init_public_params();
